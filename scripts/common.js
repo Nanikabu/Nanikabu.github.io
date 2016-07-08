8a1643e03311790b05fbe35202388b03bc6296aa
unknown <>
2016-07-08 22:54:55 +0900

    sikodotakai

--- scripts/common.js
+++ scripts/common.js
@@ -1,8 +1,14 @@
-var myButton = document.getElementById("clickButton");
-var myText = document.getElementById("helloText");
-
-myButton.addEventListener('click', doSomething, false)
-
-function doSomething() {
-	myText.textContent = "hello, world!";
-}
+$(document).ready(function(){
+  $("body").quietflow({
+  theme : "floatingBoxes",
+  specificColors : [
+    "rgba(46, 204, 113, .5)",
+    "rgba(52, 152, 219, .5)",
+    "rgba(231, 76, 60, .5)",
+    "rgba(155, 89, 182, .5)",
+    "rgba(241, 196, 15, .5)"
+  ],
+  boxCount : 100,
+  backgroundCol : "#34495E"
+  })
+})
